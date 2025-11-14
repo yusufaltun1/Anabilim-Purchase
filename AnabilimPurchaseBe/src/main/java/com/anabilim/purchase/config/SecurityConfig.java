0cb79d392ea03e318ffc52286267940050962eb0
Yusuf Altun <ysfaltn@Yusufs-Laptop.local>
2025-11-14 14:47:51 +0300

    fix

--- AnabilimPurchaseBe/src/main/java/com/anabilim/purchase/config/SecurityConfig.java
+++ AnabilimPurchaseBe/src/main/java/com/anabilim/purchase/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.anabilim.purchase.config;
 import com.anabilim.purchase.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,6 +21,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Spring Security konfigürasyonu
@@ -32,7 +34,13 @@ import java.util.Arrays;
 public class SecurityConfig {
     
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    
+
+    @Value("${client.url}")
+    private String url;
+
+    @Value("${client.url2}")
+    private String url2;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -56,10 +64,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedOrigins(List.of(url, url2));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(true); // Eğer cookie vs. gönderiyorsan true
+        configuration.setExposedHeaders(List.of("Authorization", "X-Total-Count"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
